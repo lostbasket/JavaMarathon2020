ad1f466527b54e39529ba717ff6b5f53ed0de885
Alexandr Tolstov <tolstov.inbox@gmail.com>
2020-12-25 09:27:36 +0300

    complete day1 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int i = 1;
+        while (i<11){
+            System.out.print("JAVA ");
+            i++;
+        }
     }
 }
