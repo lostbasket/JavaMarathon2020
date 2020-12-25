c5a18a666453e16122cdfb79a7e2592530951de2
Crazy_Cooler <tolstov.inbox@gmail.com>
2020-12-25 23:47:33 +0300

    complete day 2 tasks

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        a = a + 1;
+        if (a < b)
+            while (a < b) {
+                if (a % 5 == 0 && a % 10 != 0)
+                    System.out.print(a + " ");
+                a++;
+            }
+        else System.out.println("Некорректный ввод");
     }
+
 }
+
