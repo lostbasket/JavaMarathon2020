15c1a5754cea147f67a075ee74e1b1be70e2bc00
Crazy_Cooler <tolstov.inbox@gmail.com>
2020-12-27 21:10:52 +0300

    complete day3 tasks

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (i < 5) {
+            i++;
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+        }
     }
 }
+
+
+
